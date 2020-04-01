42c76a1f1670d16304a1d1194b3daa0379eb0641
Marcuss17 <eduardo.medina@utec.edu.pe>
2020-04-01 13:35:54 -0500

    Handwashing

---
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,17 @@
+package cs_utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int x = 100;
+        while(x > 0){
+            System.out.println("Debo lavarme las manos por 20 sec");
+            x--;
+        }
+    }
+}
